051610165331a1b7c597b29e8caaeedd87fe6e75
John Alvord <jalvord@us.ibm.com>
2018-01-06 08:20:35 -0800

    two utility batch files

---
+++ getidx.bat
@@ -0,0 +1,15 @@
+perl \rexx\bin\tems2sql.pl -ix -s NAME c:\ibm\itm622\kib.cat  QA1DCCT.DB  > QA1DCCT.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s ID c:\ibm\itm622\kib.cat  QA1DEVMP.DB  > QA1DEVMP.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s EVNTSERVERID c:\ibm\itm622\kib.cat  QA1DEVSR.DB  > QA1DEVSR.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s PCYNAME -s ACTNAME c:\ibm\itm622\kib.cat  QA1DACTP.DB  > QA1DACTP.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s ID  c:\ibm\itm623\kib.cat  QA1DAPPL.DB  > QA1DAPPL.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s ID  c:\ibm\itm622\kib.cat  QA1DCALE.DB  > QA1DCALE.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s ID c:\ibm\itm622\kib.cat  QA1DGRPA.DB  > QA1DGRPA.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s ID -s OBJNAME  c:\ibm\itm622\kib.cat  QA1DGRPI.DB  > QA1DGRPI.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s ID c:\ibm\itm622\kib.cat  QA1DNAME.DB  > QA1DNAME.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix  -s NODE -s NODELIST c:\ibm\itm622\kib.cat  QA1CNODL.DB  > QA1CNODL.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s OBJNAME -s NODEL c:\ibm\itm622\kib.cat  QA1DOBJA.DB  > QA1DOBJA.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s ID c:\ibm\itm622\kib.cat  QA1DOVRI.DB  > QA1DOVRI.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s ID -s ITEMID c:\ibm\itm622\kib.cat  QA1DOVRD.DB  > QA1DOVRD.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix -s PCYNAMEc:\ibm\itm622\kib.cat  QA1DPCYF.DB  > QA1DPCYF.DB.IDX
+perl \rexx\bin\tems2sql.pl -ix-s SITNAME c:\ibm\itm622\kib.cat  QA1CSITF.DB  > QA1CSITF.DB.IDX
---
+++ getmissd.bat
@@ -0,0 +1,15 @@
+perl \rexx\bin\tems2sql.pl -s NAME -e -sx QA1DCCT.DB.IDX c:\ibm\itm622\kib.cat  QA1DCCT.DB  > QA1DCCT.DB.SQL
+perl \rexx\bin\tems2sql.pl -s ID -e -sx QA1DEVMP.DB.IDX c:\ibm\itm622\kib.cat  QA1DEVMP.DB  > QA1DEVMP.DB.SQL
+perl \rexx\bin\tems2sql.pl -s EVNTSERVERID -e -sx QA1DEVSR.DB.IDX c:\ibm\itm622\kib.cat  QA1DEVSR.DB  > QA1DEVSR.DB.SQL
+perl \rexx\bin\tems2sql.pl -s PCYNAME -s ACTNAME -e -sx QA1DACTP.DB.IDX c:\ibm\itm622\kib.cat  QA1DACTP.DB  > QA1DACTP.DB.SQL
+perl \rexx\bin\tems2sql.pl -s ID -e -sx QA1DAPPL.DB.IDX  c:\ibm\itm623\kib.cat  QA1DAPPL.DB  > QA1DAPPL.DB.SQL
+perl \rexx\bin\tems2sql.pl -l -s ID -e -sx QA1DCALE.DB.IDX c:\ibm\itm622\kib.cat  QA1DCALE.DB  > QA1DCALE.DB.SQL
+perl \rexx\bin\tems2sql.pl -s ID  -e -sx QA1DGRPA.DB.IDX c:\ibm\itm622\kib.cat  QA1DGRPA.DB  > QA1DGRPA.DB.SQL
+perl \rexx\bin\tems2sql.pl -s ID -s OBJNAME  -e -sx QA1DGRPI.DB.IDX c:\ibm\itm622\kib.cat  QA1DGRPI.DB  >QA1DGRPI.DB.SQL
+perl \rexx\bin\tems2sql.pl -s ID -e -sx QA1DNAME.DB.IDX   c:\ibm\itm622\kib.catQA1DNAME.DB  > QA1DNAME.DB.SQL
+perl \rexx\bin\tems2sql.pl   -s NODE -s NODELIST -e -sx QA1CNODL.DB.IDX  c:\ibm\itm622\kib.catQA1CNODL.DB  > QA1CNODL.DB.SQL
+perl \rexx\bin\tems2sql.pl -s OBJNAME -s NODEL -e -sx QA1DOBJA.DB.IDX c:\ibm\itm622\kib.cat  QA1DOBJA.DB  > QA1DOBJA.DB.SQL
+perl \rexx\bin\tems2sql.pl -s ID -e -sx QA1DOVRI.DB.IDX c:\ibm\itm622\kib.cat  QA1DOVRI.DB  > QA1DOVRI.DB.SQL
+perl \rexx\bin\tems2sql.pl -s ID-s ITEMID   -e -sx  QA1DOVRD.DB.IDX c:\ibm\itm622\kib.cat    QA1DOVRD.DB  > QA1DOVRD.DB.SQL
+perl \rexx\bin\tems2sql.pl -l -s PCYNAME -e -sx  QA1DPCYF.DB.IDX c:\ibm\itm622\kib.cat QA1DPCYF.DB  > QA1DPCYF.DB.SQL
+perl \rexx\bin\tems2sql.pl -s SITNAME -e -sx  QA1CSITF.DB.IDX c:\ibm\itm622\kib.cat  QA1CSITF.DB  > QA1CSITF.DB.SQL
